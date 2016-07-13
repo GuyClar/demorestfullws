5880d5001c7365c15c7fb80c7a9a2da3663ec02a
GuyClar <oliviergisclard@gmail.com>
2016-07-13 11:13:35 +0200

    Add files via upload

---
+++ src/com/guyclar/demorest/HelloWorldService.java
@@ -0,0 +1,22 @@
+package com.guyclar.demorest;
+
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+ 
+@Path("/hello")
+public class HelloWorldService {
+ 
+	@GET
+	@Path("/{param}")
+	public Response getMsg(@PathParam("param") String msg) {
+ 
+		String output = "Jersey reponds : " + msg;
+ 
+		return Response.status(200).entity(output).build();
+ 
+	}
+ 
+}
